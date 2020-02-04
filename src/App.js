922801798d58854c73fa3ed1f50d74a7e8858792
DustinG98 <thedustingraham@gmail.com>
2020-02-03 20:42:13 -0700

    init, setup store and 'empty' reducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+
+
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
+
+const App = props => {
   const state = {
     additionalPrice: 0,
     car: {
